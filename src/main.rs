131d16807c0b450ab075c5240797cdafd0af7c95
Kyle Pfromer <kpfromer2@gmail.com>
2022-11-17 12:14:19 -0700

    panic turns off motor

--- src/main.rs
+++ src/main.rs
@@ -303,9 +303,14 @@ mod app {
 fn panic(_: &core::panic::PanicInfo) -> ! {
     let device = unsafe { hal::pac::Peripherals::steal() };
 
+    let port0 = hal::gpio::p0::Parts::new(device.P0);
     let port1 = hal::gpio::p1::Parts::new(device.P1);
+
+    let mut motor = Led::new(port0.p0_03.degrade());
     let mut red_led = Led::new(port1.p1_01.degrade());
 
+    // MAKE SURE TO TURN OFF MOTOR IF ANYTHING GOES WRONG
+    motor.off();
     red_led.on();
 
     loop {
